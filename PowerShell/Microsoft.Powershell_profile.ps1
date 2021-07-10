ad7c8147bc522dec2626bd0b4018bc9c47931f1e
Harish <harishkalva4126@gmail.com>
2021-07-10 12:42:00 +0530

    tab autocompletion
    Signed-off-by: Harish <harishkalva4126@gmail.com>

--- PowerShell/Microsoft.Powershell_profile.ps1
+++ PowerShell/Microsoft.Powershell_profile.ps1
@@ -2,3 +2,9 @@ Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "$HOME\.starship\config.toml"
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
